3af7a360611156b4d5c45d09d26d91432cdad0b1
ricky <rickyp72@googmail.com>
2014-02-04 09:31:00 +0000

    no message

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -3,20 +3,13 @@ class people::rickyp72 {
   #include vagrant
   include sublime_text_2
   include skype
-  #include iterm2
-<<<<<<< HEAD
+  ## include iterm2
   # include evernote
   # include firefox
   # include chrome
   # include googledrive
   # include dropbox
-=======
-  # include evernote
-  # include firefox
-  # include chrome
-  # include googledrive
-  # include dropbox
->>>>>>> be2ef57d55e23d907c79018bc41e02a7f66dc45e
   # include vmware_fusion
  # include virtualbox
+ 
 }
