2a01e8e3d2c991af98234c6d036e829259a0749f
ricky <rickyp72@googmail.com>
2014-02-13 09:13:01 +0000

    no message

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -1,22 +1,14 @@
 class people::rickyp72 {
   notify { 'Hello Ricky': }
-  #include vagrant
-  include sublime_text_2
-  include skype
-  ## include iterm2
+  # include vagrant
+  # include sublime_text_2
+  # include skype
+  # include iterm2
   # include evernote
-  include firefox
-  include chrome
+  # include firefox
+  # include chrome
   # include googledrive
   # include dropbox
   # include vmware_fusion
- # include virtualbox
-    class { 'osx::global::key_repeat_delay':
-    delay => 200
-  }
-
-  class { 'osx::global::key_repeat_rate':
-    rate => 400
-  }
-
+  # include virtualbox
 }
