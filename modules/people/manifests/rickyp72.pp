0933ec618b0ffdc6e604770087eb59d2f286f769
ricky <rickyp72@googmail.com>
2014-02-13 09:16:53 +0000

    no message

--- modules/people/manifests/rickyp72.pp
+++ modules/people/manifests/rickyp72.pp
@@ -2,13 +2,13 @@ class people::rickyp72 {
   notify { 'Hello Ricky': }
   # include vagrant
   # include sublime_text_2
-  # include skype
+   include skype
   # include iterm2
   # include evernote
   # include firefox
   # include chrome
   # include googledrive
-  # include dropbox
+   include dropbox
   # include vmware_fusion
   # include virtualbox
 }
