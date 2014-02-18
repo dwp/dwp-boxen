5bfcf1f4fa9a478adc988018b01508b7d581aecd
ricky <rickyp72@googmail.com>
2014-02-18 16:19:30 +0000

    changed login items

--- modules/people/manifests/rickyp72/loginitems.pp
+++ modules/people/manifests/rickyp72/loginitems.pp
@@ -4,25 +4,25 @@ class people::rickyp72::loginitems (
   	$my_username  = $people::rickyp72::params::my_username
 	){
 	
-	osx_login_item { 'Alfred 2':
-		name => 'Alfred 2',
-		path => '/Applications/Alfred 2.app',
-		hidden => true,
-		require => Class['alfred'],
+	osx_login_item { 'TextEdit':
+		name => 'TextEdit',
+		path => '/Applications/TextEdit.app',
+		hidden => false,
+		# require => Class['alfred'],
 	}
 		
 		
-	osx_login_item { 'Bartender':
-		name => 'Bartender',
-		path => '/Applications/Bartender.app',
-		hidden => true,
-		require => Package['Bartender'],
-	}	
+	# osx_login_item { 'Bartender':
+	# 	name => 'Bartender',
+	# 	path => '/Applications/Bartender.app',
+	# 	hidden => true,
+	# 	require => Package['Bartender'],
+	# }	
 		
-	osx_login_item { 'Dropbox':
-	   name => 'Dropbox',
-	   path => '/Applications/Dropbox.app',
-	   hidden => true,
-	   require => Class['dropbox'],
+	# osx_login_item { 'Dropbox':
+	#    name => 'Dropbox',
+	#    path => '/Applications/Dropbox.app',
+	#    hidden => true,
+	#    require => Class['dropbox'],
 	}
 }
