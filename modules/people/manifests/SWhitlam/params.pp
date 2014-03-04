e213b3d2e19692e18a92bfb286bbc6e6cc1488f2
Stuart Whitlam <Stuart@Stuarts-MacBook-Pro.local>
2014-03-04 14:01:07 +0000

    added more manifests

---
+++ modules/people/manifests/SWhitlam/params.pp
@@ -0,0 +1,6 @@
+class people::rickyp72::params {
+  # $::luser and $::boxen_srcdir come from Boxen's custom facts
+  $my_username  = $::luser
+  $my_homedir   = "/Users/${::luser}"
+  $my_sourcedir = $::boxen_srcdir
+}
