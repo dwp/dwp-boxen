7abd49dc43b1ebb2ad544000afc4e80644eee964
Stuart Whitlam <Stuart@Stuarts-MacBook-Pro.local>
2014-03-04 12:12:12 +0000

    new manifest

---
+++ modules/people/manifests/SWhitlam.pp
@@ -0,0 +1,17 @@
+class people::rickyp72 {
+  notify { 'Hello Ricky': }
+  # include people::rickyp72::params
+  # include people::rickyp72::gems
+  # include people::rickyp72::loginitems
+  # include vagrant
+  # include sublime_text_2
+   include skype
+  # include iterm2
+  # include evernote
+  # include firefox
+  # include chrome
+  # include googledrive
+   include dropbox
+  # include vmware_fusion
+  # include virtualbox
+}
