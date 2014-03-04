75910a0b54b1e1595dd7e919597ecb3a9becef3f
Kevin Adams <kevinadams@Kevins-MacBook-Pro.local>
2014-03-04 12:30:23 +0000

    added new class

---
+++ modules/people/manifests/kadams.pp
@@ -0,0 +1,17 @@
+class people::kadams {
+  notify { 'Hello Kev': }
+  # include people::rickyp72::params
+  # include people::rickyp72::gems
+  # include people::rickyp72::loginitems
+  # include vagrant
+  # include sublime_text_2
+   include skype
+  # include iterm2
+  # include evernote
+  # include firefox
+  # include chrome
+  # include googledrive
+   include dropbox
+  # include vmware_fusion
+  # include virtualbox
+}
